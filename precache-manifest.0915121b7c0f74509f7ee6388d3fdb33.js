ecf7d6796b3000c9b7667cd13a2572a0fe282cbe
Austin Burnett <tinz1320@gmail.com>
2020-08-22 01:44:38 -0600

    Updates

---
+++ precache-manifest.0915121b7c0f74509f7ee6388d3fdb33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0965cfa9ae3ffed0c238a1f033f3a04",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "0bdba5f5d18afb6269bc",
+    "url": "/clickygame/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "276a5d87f4ace85d4171",
+    "url": "/clickygame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0bdba5f5d18afb6269bc",
+    "url": "/clickygame/static/js/2.35dd075e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.35dd075e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "276a5d87f4ace85d4171",
+    "url": "/clickygame/static/js/main.0bf1c789.chunk.js"
+  },
+  {
+    "revision": "1bde1f52d264e4573c62",
+    "url": "/clickygame/static/js/runtime-main.b87772f1.js"
+  }
+]);
