ecc0ab6214ecf65f9f8e878562b4e98f527dd805
Austin Burnett <tinz1320@gmail.com>
2020-08-22 01:21:47 -0600

    Finalized look

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import GameCard from "./components/GameCard";
+import matches from "./MatchCards";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    matches,
+    correctGuesses: 0,
+    bestScore: 0,
+    clickMessage: "Try to click on each hero once! Two clicks on the same hero means game over!"
+  };
+
+  handleImageClicked = async id => {
+    const moviePosters = document.getElementById("game-cards");
+    const clickedMatch = this.state.matches.filter(match => match.id === id);
+    moviePosters.classList.remove("apply-shake");
+    if(clickedMatch[0].clicked) {
+
+      moviePosters.classList.add("apply-shake");
+      this.setState({
+        correctGuesses: 0,
+        clickMessage:"You already clicked on that one. Try again.",
+      });
+      this.handleResetAllCards();
+    } else if(this.state.correctGuesses < 11) {
+      clickedMatch[0].clicked = true;
+      this.setState({correctGuesses: this.state.correctGuesses + 1}, () => {
+        if (this.state.correctGuesses > this.state.bestScore){
+          this.setState({ bestScore: this.state.correctGuesses });
+        }
+      });
+      this.setState({clickMessage:  "Scored a point! Great work!" });
+      this.handleShuffleCards();
+    } else {
+      clickedMatch[0].clicked = true;
+      this.state.correctGuesses = 0;
+      this.state.clickMessage = "All heroes correct?? Great job!!";
+      this.setState({ bestScore: 12 });
+      this.handleResetAllCards();
+    }
+  };
+
+  handleShuffleCards = () => {
+    let cards = this.state.matches;
+    for (let i = 0; i < cards.length - 1; i++) {
+      const j = i + Math.floor(Math.random() * (cards.length - i));
+
+      const temp = cards[j];
+      cards[j] = cards[i];
+      cards[i] = temp;
+    }
+    this.setState({matches: cards});
+  };
+
+  handleResetAllCards = () => {
+    for (let i = 0 ; i < this.state.matches.length ; i++){
+      matches[i].clicked = false;
+      this.setState({
+        correctGuesses: 0,
+        matches: matches
+      });
+      this.handleShuffleCards()
+    }
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <Navbar score={this.state.correctGuesses} topScore={this.state.bestScore} />
+          <div className="jumbotron py-3">
+            <h4>{this.state.clickMessage}</h4>
+          </div>
+          <div className="container">
+            <div id="game-cards" className="row">
+              {this.state.matches.map((matchCard, index) => {
+                return <GameCard
+                    key={index}
+                    id={matchCard.id}
+                    name={matchCard.name}
+                    image={matchCard.image}
+                    handleOnClick={this.handleImageClicked}
+                />
+              })}
+            </div>
+
+          </div>
+        </div>
+    );
+  };
 }
 
 export default App;
